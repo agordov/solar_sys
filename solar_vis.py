5bc53f0f126fe26e2cca636206e96260b6e148bb
Krivchuk Vasily <krivchuk.vo@phystech.edu>
2021-11-11 19:33:49 +0300

    Сделано отображение планет

--- solar_vis.py
+++ solar_vis.py
@@ -74,7 +74,7 @@ class Drawer:
     def update(self, figures, ui):
         self.screen.fill('white')
         for figure in figures:
-            figure.draw(self.screen)
+            pg.draw.circle(self.screen, figure.color, (scale_x(figure.x), scale_y(figure.y)), figure.R)
         
         ui.blit()
         ui.update()
@@ -89,5 +89,5 @@ class DrawableObject:
         self.x = obj.x
         self.y = obj.y
 
-    def draw(self, surface):
-        pg.draw.circle(surface, self.color, (self.x, self.y), self.R)  
+    def draw(self, surface, obj):
+        pg.draw.circle(surface, self.color, (self.x, self.y), self.R)
